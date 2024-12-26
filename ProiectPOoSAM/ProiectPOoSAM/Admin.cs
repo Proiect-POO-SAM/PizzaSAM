789b4583c66ecc568b51bbf9e292a78e9e2dd0eb
SebastianAdelin <sebastian.covaciu@student.upt.ro>
2024-12-26 22:54:15 +0200

    Renuntare clase Admin si Client

--- ProiectPOoSAM/ProiectPOoSAM/Admin.cs
+++
@@ -1,8 +0,0 @@
-using ProiectPOOSAM;
-
-namespace ProiectPOoSAM;
-
-public class Admin
-{
-    //public Admin(string username, string password,Role role) : base(username, password, Role.Admin) { }
-}
